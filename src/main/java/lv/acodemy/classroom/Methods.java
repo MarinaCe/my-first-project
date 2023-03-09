d2597941bc39d03793555876f3fb4d3841e8206e
marina.celika <marina.celika@gmail.com>
2023-03-09 20:49:39 +0200

    [classroom_09032023]: Add classroom project

---
+++ src/main/java/lv/acodemy/classroom/Methods.java
@@ -0,0 +1,18 @@
+package lv.acodemy.classroom;
+
+public class Methods {
+    public static void main(String[] args) {
+        printText();
+    }
+    public static void printText(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+    }
+    /* printText("My name is Marina");
+    public static void printText(String text){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(text);
+
+     */
+}
